952bd08b3bb40a5fb99893c8096d92210bc5c27a
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2024-11-14 10:50:50 -0500

    Completed 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(foreString) {
+    inputLength = foreString.length;
+    let currentLetter = inputLength - 1;
+    let backString = "";
+    while (inputLength) {
+        backString += foreString.charAt(currentLetter);
+        foreString = foreString.slice(0, -1);
+        currentLetter = foreString.length - 1;
+        inputLength --;
+    }
+    return backString;
 };
 
 // Do not edit below this line
