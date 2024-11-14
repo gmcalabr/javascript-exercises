ecacf290f5f1248b2e928024264ecc470dbe1f32
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2024-11-14 18:19:13 -0500

    Completed 05_sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
 
+    let answer = 0;
+    let numArray = [];
+
+    if (!Number.isInteger(low) || !Number.isInteger(high)) return "ERROR";
+    if (low < 0 || high < 0) return "ERROR";
+
+    if (low > high) for (let i = high; i <= low; i++) numArray.push(i);
+    if (low <= high) for (let i = low; i <= high; i++) numArray.push(i);
+
+    numArray.forEach((item) => answer += item);
+
+    return answer;
 };
 
 // Do not edit below this line
