2743092581374a81dea2405361df29336e1746f6
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2024-11-15 11:20:44 -0500

    Completed 06_leapyears exercise
    I completed the assignment pretty easily but did not consider combining
    ladder logic until seeing the solution. Took me a bit to work out a
    basic layout but still had difficulty with (! || ) logic. Working on
    that would help me.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivByFour = !Boolean(year % 4);
+    const isCentury = !Boolean(year % 100);
+    const isQuadriCentennial = !Boolean(year % 400);
 
+    if ( isDivByFour && 
+        (!isCentury || isQuadriCentennial)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
