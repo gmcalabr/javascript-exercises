2667fc25779d3a68f8632972f554d6bb5341b8dc
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2024-11-15 11:37:57 -0500

    Completed 07_tempConversion exercise
    I see where the solution is shorter but it's also harder to read. Going
    to keep my solution. Math.round is probably good to have under my belt
    instead .toFixed(), but right now I'm not seeing a downside.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (parseFloat(farenheit) - 32)* (5/9);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = (parseFloat(celsius) * (9/5) + 32);
+  return parseFloat(farenheit.toFixed(1));
 };
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
