b0b55d8d3550d4509329c3e00879c68f8ccc0c77
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2025-01-09 18:21:25 -0500

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
-};
+const add = (a, b) => (a + b);
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => (a - b);
 
-const sum = function() {
-	
-};
+const sum = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+}
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+}
 
-};
+const power = (a, b) => (a ** b);
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  let final = 1;
+  let myArray = [];
+  if (num != 0) {
+    for (let i = num; i > 0; i--) {
+      myArray.push(i);
+    };
+    final = myArray.reduce((total, currentItem) => {
+      return total*currentItem;
+    }, 1);
+  }
+  return final;
+}
 
 // Do not edit below this line
 module.exports = {
