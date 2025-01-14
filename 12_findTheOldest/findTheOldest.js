49d4dcdc8f4390eff723db40ebc26b7244878b7f
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2025-01-14 14:26:56 -0500

    Completed exercise 12_findTheOldest
    I think my sort() method is cleaner and more straightforward than the
    given suggested reduce() method, although I recognize that separating
    the function into two clear steps (two clear functions) is possibly a
    good idea.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+        for (let person of people) {
+            if (!person.yearOfDeath) {
+                person.yearOfDeath = ((new Date()).getFullYear());
+            };
+        };
+        
+        let byOldest = people.sort((a, b) => {
+            let ageOne = (a.yearOfDeath - a. yearOfBirth)
+            let ageTwo = ((b.yearOfDeath - b. yearOfBirth))
+            return (ageOne > ageTwo) ? -1 : 1 ;
+        });
+        
+    return byOldest[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
